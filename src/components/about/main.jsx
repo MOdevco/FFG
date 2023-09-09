95f8def3754e33cde402fd9ad583c9311e4ef6b3
Hikmatillo019 <rayimjonovhikmatillo3@gmail.com>
2023-09-09 09:23:15 +0500

    main

---
+++ src/components/about/main.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+
+function Main() {
+  return (
+    <Box>
+      <Box textAlign={'center'} fontSize={'40px'} color={'#0B7077'} fontWeight={'700'} mb={'71px'} >
+        <h1>FULL FORCE GROUP KOMPANIYASI TARIXI</h1>
+      </Box>
+      <Box className="text"   w={'80%'} h={'110vh'} m={'auto'}   border={'1px solid '} borderTop={'  30px solid #0B7077'}  >
+        <Box w={'80%'} m={'auto'} py={'30px'}  >
+        <Text  fontSize={'30px'} fontWeight={'500'} >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+      
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+       
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis esse illo quis in ex cum commodi consectetur aliquam. Dicta tempora hic, quidem architecto cupiditate quaerat nulla iure tempore eum. Voluptas.
+        <br />
+        <br />
+      
+
+        </Text>
+           
+            
+            
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Main;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/about/navbar'
+import Main from '../components/about/main'
 
 function About() {
   return (
     <div>
       <Navbar />
+      <Main/>
     </div>
   )
 }
